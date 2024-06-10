fcff6d5f8d6a00c522f300264a1b6a8ca9d1536d
Jonathas Menezes <jonathassk8@gmail.com>
2024-06-10 20:08:32 -0300

    Começando o front do projeto. Criando a pagina de login.

--- src/main/java/com/sgspproject/sgsp/Sgsp.java
+++ src/main/java/com/sgspproject/sgsp/Sgsp.java
@@ -1,21 +1,18 @@
 package com.sgspproject.sgsp;
 
-import com.sgspproject.sgsp.model.entity.Professor;
+import com.sgspproject.sgsp.view.LoginPage;
 
 public class Sgsp {
 
     public static void main(String[] args) {
         
-        Professor pf = new Professor("jorge", "maria@maria.com.br", 2222);
-        System.out.println("O professor cadastrado foi: " + pf.getNome());
-        System.out.println("Com o email: " + pf.getEmail());
-        
-//        JFrame jf = new JFrame();
-//        Login login = new Login();
-//        jf.add(login);
-//        jf.setSize(700, 500);
-//        jf.setLocationRelativeTo(null);
-//        jf.setDefaultCloseOperation(EXIT_ON_CLOSE);
-//        jf.setVisible(true);
+//        Professor pf = new Professor("jorge", "maria@maria.com.br", 2222);
+//        System.out.println("O professor cadastrado foi: " + pf.getNome());
+//        System.out.println("Com o email: " + pf.getEmail());
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginPage().setVisible(true);
+            }
+        });
     }
 }
